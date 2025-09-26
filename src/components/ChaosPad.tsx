6834168b45cd2ec2e44c893b2f8a9442beca2140
kohey94 <sugawara622.works@gmail.com>
2025-09-27 02:19:41 +0900

    fix

--- src/components/ChaosPad.tsx
+++ src/components/ChaosPad.tsx
@@ -27,8 +27,8 @@ const ChaosPad: React.FC<ChaosPadProps> = ({ size = 300, onChange }) => {
 
     // 1オクターブの音階（C4〜C5）
     const notes = [
-        "C4", "C#4", "D4", "D#4", "E4", "F4",
-        "F#4", "G4", "G#4", "A4", "A#4", "B4", "C5"
+        "C5", "C#5", "D5", "D#5", "E5", "F5",
+        "F#5", "G5", "G#5", "A5", "A#5", "B5", "C6"
     ];
 
     if (!synthRef.current) {
